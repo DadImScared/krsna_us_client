b37ec5af23c94db5d9e023eec0f46a071ca82373
Matthew Bernheimer <Krutonslol@gmail.com>
2018-03-06 17:17:30 -0500

    better display error message in password validator

--- src/components/Register/validators.js
+++ src/components/Register/validators.js
@@ -3,8 +3,8 @@ export const passwordValidator = (props, { form: { password1, password2 }, formE
   const newErrors = { ...formErrors };
   console.log('in validator', password1, password2);
   if (password1 !== password2 && password2 && password1) {
-    newErrors['password1'] = 'passwords must match';
-    newErrors['password2'] = 'passwords must match';
+    newErrors['password1'] = 'Passwords must match';
+    newErrors['password2'] = 'Passwords must match';
   }
   else {
     newErrors['password1'] = '';
@@ -12,7 +12,7 @@ export const passwordValidator = (props, { form: { password1, password2 }, formE
   }
   if (password1.length && password1.length < 8) {
     if (newErrors['password1']) {
-      newErrors['password1'] += 'Password must be at least 8 characters long';
+      newErrors['password1'] += ' Password must be at least 8 characters long';
     }
     else {
       newErrors['password1'] = 'Password must be at least 8 characters long';
